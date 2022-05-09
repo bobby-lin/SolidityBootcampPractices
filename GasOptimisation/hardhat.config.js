14f0bd631a040cb260a1bc183c3250e94a2658a8
Bobby Lin <mr.linjiahao@gmail.com>
2022-05-09 22:26:15 +0800

    Update hardhat.config.js

--- GasOptimisation/hardhat.config.js
+++ GasOptimisation/hardhat.config.js
@@ -16,15 +16,24 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
+ module.exports = {
   solidity: {
     compilers: [
       {
         version: "0.8.0",
+
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 300,
+          },
+        },
       },
     ],
   },
   gasReporter: {
     currency: "USD",
+    gasPrice: 6.4,
+    token: "ETH",
   },
 };
